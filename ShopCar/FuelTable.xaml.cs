ded8d2b5d68a5687881acd9d2a48423d9b2cf7c3
TereshkovychYura <tereskovychura@gmail.com>
2019-06-04 10:13:15 +0300

    Create Window for table

--- ShopCar/FuelTable.xaml.cs
+++ ShopCar/FuelTable.xaml.cs
@@ -10,18 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data.SQLite;
 
 namespace ShopCar
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FuelTable.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FuelTable : Window
     {
-        public MainWindow()
+        public FuelTable()
         {
             InitializeComponent();
         }
