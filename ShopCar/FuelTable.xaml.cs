5103ec8c5aa340ba2bcd7e212da91fa8a63f51c9
TereshkovychYura <tereskovychura@gmail.com>
2019-06-04 11:01:43 +0300

    add

--- ShopCar/FuelTable.xaml.cs
+++ ShopCar/FuelTable.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,48 @@ namespace ShopCar
     /// </summary>
     public partial class FuelTable : Window
     {
+        private string tblFuels = "tblSFuels";
         public FuelTable()
         {
             InitializeComponent();
+        }
+        private void BtnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            string dbName = "";
+            SQLiteConnection con = new SQLiteConnection($"Data Source={dbName}");
+            con.Open();
+            GenerateTabel(con);
+            Seed(con);
+            con.Close();
+        }
+        private void GenerateTabel(SQLiteConnection con)
+        {
+            string query = $"CREATE TABLE IF NOT EXISTS {tblFuels} " +
+                                "(Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                    "FuelType TEXT NOT NULL " +
+                                ");";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+            cmd.ExecuteNonQuery();
+        }
+        private void Seed(SQLiteConnection con)
+        {
+            #region SeedFuels
+            string query = $"Insert into {tblFuels}(FuelType) " +
+                $"values('ELectro');";
+
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+            cmd.ExecuteNonQuery();
+
+            query = $"Insert into {tblFuels}(FuelType) " +
+                $"values('Disel');";
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+
+            query = $"Insert into {tblFuels}(FuelType) " +
+                $"values('AirFuel');";
+            cmd.CommandText = query;
+            cmd.ExecuteNonQuery();
+            #endregion
         }
     }
 }
